a2cce7a38736b585216090e220ef40b46be18c8b
Paldeep Singh <paldeepsingh21@gmail.com>
2025-01-16 20:05:01 +1100

    Add eslint

---
+++ mobile-app/eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    plugins: {
+      "react-hooks": eslintPluginReactHooks,
+    },
+    rules: { ...eslintPluginReactHooks.configs.recommended.rules },
+  }
+);
