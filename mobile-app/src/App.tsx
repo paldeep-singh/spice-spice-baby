e446a8e733bedd90bfb70a1c4a699d4b4f434a07
Paldeep Singh <paldeepsingh21@gmail.com>
2025-01-16 21:02:32 +1100

    Add gesture handler

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -5,43 +5,46 @@ import { theme, ThemeContext } from "./utils/theme";
 import { HomeScreen } from "./screens/Home";
 import { SearchScreen } from "./screens/Search";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const Tab = createBottomTabNavigator();
 
 export function App() {
   return (
-    <ThemeContext.Provider value={theme}>
-      <NavigationContainer>
-        <SafeAreaProvider>
-          <Tab.Navigator
-            screenOptions={{
-              headerStyle: { backgroundColor: theme.background.base },
-              headerTitleStyle: { color: theme.text.base },
-              tabBarStyle: {
-                backgroundColor: theme.background.light,
-                borderTopColor: theme.background.light,
-              },
-              tabBarActiveTintColor: theme.text.selected,
-              animation: "shift",
-            }}
-            screenLayout={({ children }) => (
-              <SafeAreaView
-                style={{
-                  backgroundColor: theme.background.base,
-                  flex: 1,
-                }}
-              >
-                {children}
-              </SafeAreaView>
-            )}
-          >
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Search" component={SearchScreen} />
-          </Tab.Navigator>
+    <GestureHandlerRootView>
+      <ThemeContext.Provider value={theme}>
+        <NavigationContainer>
+          <SafeAreaProvider>
+            <Tab.Navigator
+              screenOptions={{
+                headerStyle: { backgroundColor: theme.background.base },
+                headerTitleStyle: { color: theme.text.base },
+                tabBarStyle: {
+                  backgroundColor: theme.background.light,
+                  borderTopColor: theme.background.light,
+                },
+                tabBarActiveTintColor: theme.text.selected,
+                animation: "shift",
+              }}
+              screenLayout={({ children }) => (
+                <SafeAreaView
+                  style={{
+                    backgroundColor: theme.background.base,
+                    flex: 1,
+                  }}
+                >
+                  {children}
+                </SafeAreaView>
+              )}
+            >
+              <Tab.Screen name="Home" component={HomeScreen} />
+              <Tab.Screen name="Search" component={SearchScreen} />
+            </Tab.Navigator>
 
-          <StatusBar style="inverted" />
-        </SafeAreaProvider>
-      </NavigationContainer>
-    </ThemeContext.Provider>
+            <StatusBar style="inverted" />
+          </SafeAreaProvider>
+        </NavigationContainer>
+      </ThemeContext.Provider>
+    </GestureHandlerRootView>
   );
 }
