3f49b207187b6de52af5dc093661adb6088fca6d
Paldeep Singh <paldeepsingh21@gmail.com>
2025-01-16 21:00:01 +1100

    Add shift animation

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { theme, ThemeContext } from "./utils/theme";
 import { HomeScreen } from "./screens/Home";
 import { SearchScreen } from "./screens/Search";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,22 +12,35 @@ export function App() {
   return (
     <ThemeContext.Provider value={theme}>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: theme.background.base },
-            headerTitleStyle: { color: theme.text.base },
-            tabBarStyle: {
-              backgroundColor: theme.background.light,
-              borderTopColor: theme.background.light,
-            },
-            tabBarActiveTintColor: theme.text.selected,
-          }}
-        >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
-        </Tab.Navigator>
+        <SafeAreaProvider>
+          <Tab.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: theme.background.base },
+              headerTitleStyle: { color: theme.text.base },
+              tabBarStyle: {
+                backgroundColor: theme.background.light,
+                borderTopColor: theme.background.light,
+              },
+              tabBarActiveTintColor: theme.text.selected,
+              animation: "shift",
+            }}
+            screenLayout={({ children }) => (
+              <SafeAreaView
+                style={{
+                  backgroundColor: theme.background.base,
+                  flex: 1,
+                }}
+              >
+                {children}
+              </SafeAreaView>
+            )}
+          >
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Search" component={SearchScreen} />
+          </Tab.Navigator>
 
-        <StatusBar style="inverted" />
+          <StatusBar style="inverted" />
+        </SafeAreaProvider>
       </NavigationContainer>
     </ThemeContext.Provider>
   );
