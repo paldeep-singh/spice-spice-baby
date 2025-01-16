5c1b25592cea7d6bad1def33d942ad20c4a3afa5
Paldeep Singh <paldeepsingh21@gmail.com>
2025-01-16 18:08:06 +1100

    Add Home adn search tabs

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,31 +1,43 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Home"
-        component={() => (
-          <View style={styles.container}>
-            <Text>Spice Spice Baby Home Screen</Text>
-            <StatusBar style="auto" />
-          </View>
-        )}
-      />
-      <Tab.Screen
-        name="Search"
-        component={() => (
-          <View style={styles.container}>
-            <Text>Spice Spice Baby Search Screen</Text>
-            <StatusBar style="auto" />
-          </View>
-        )}
-      />
-    </Tab.Navigator>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#22262E" },
+          headerTitleStyle: { color: "#ffffff" },
+          tabBarStyle: {
+            backgroundColor: "#323232",
+            borderTopColor: "#323232",
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={() => (
+            <View style={styles.container}>
+              <Text style={styles.text}>Spice Spice Baby Home Screen</Text>
+              <StatusBar style="auto" />
+            </View>
+          )}
+        />
+        <Tab.Screen
+          name="Search"
+          component={() => (
+            <View style={styles.container}>
+              <Text style={styles.text}>Spice Spice Baby Search Screen</Text>
+              <StatusBar style="auto" />
+            </View>
+          )}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -35,5 +47,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#22262E",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    color: "#ffffff",
   },
 });
