8357fcf88b72980f9e4bb77db4f12649bd3e8803
Paldeep Singh <paldeepsingh21@gmail.com>
2025-01-16 17:56:59 +1100

    Add tab navigator

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,19 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Spice Spice Baby </Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={() => (
+          <View style={styles.container}>
+            <Text>Spice Spice Baby Home Screen</Text>
+            <StatusBar style="auto" />
+          </View>
+        )}
+      />
+      <Tab.Screen
+        name="Search"
+        component={() => (
+          <View style={styles.container}>
+            <Text>Spice Spice Baby Search Screen</Text>
+            <StatusBar style="auto" />
+          </View>
+        )}
+      />
+    </Tab.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#22262E",
     alignItems: "center",
     justifyContent: "center",
   },
